902b60539da8ee7d5ece1395a52f7bed9ec3d2de
Amelia Elton <ameliareneeelton@gmail.com>
2020-09-11 17:32:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,5 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
-   puts "Fizz"
-  else if int % 5 == 0
-   puts "Buzz"
-  else if int % 15 == 0
-    puts "fizzbuzz"
-  else 
-    puts nil
+  
 end
